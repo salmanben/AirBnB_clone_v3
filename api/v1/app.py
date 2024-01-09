1c4d6d54c0ff9be2c0a9fb7775785332e91e80d9
SALMAN BEN OMAR <salmanbenomar250@gmail.com>
2024-01-09 01:16:05 +0100

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,39 +1,42 @@
 #!/usr/bin/python3
-""" Flask Application """
+'''
+Createw Flask app; and register the blueprint app_views to Flask instance app.
+'''
+
+from os import getenv
+from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
-from os import getenv
-from flask import Flask, render_template, make_response, jsonify
-from flask_cors import CORS
 
 app = Flask(__name__)
+
+# enable CORS and allow for origins:
+CORS(app, resources={r'/api/v1/*': {'origins': '0.0.0.0'}})
+
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def close_db(error):
-    """ Close Storage """
+def teardown_engine(exception):
+    '''
+    Removes the current SQLAlchemy Session object after each request.
+    '''
     storage.close()
 
 
+# Error handlers for expected app behavior:
 @app.errorhandler(404)
 def not_found(error):
-    """ 404 Error
-    ---
-    responses:
-      404:
-        description: a resource was not found
-    """
+    '''
+    Return errmsg `Not Found`.
+    '''
     response = {'error': 'Not found'}
     return jsonify(response), 404
 
 
-
-if __name__ == "__main__":
-    """ Main Function """
+if __name__ == '__main__':
     HOST = getenv('HBNB_API_HOST', '0.0.0.0')
     PORT = int(getenv('HBNB_API_PORT', 5000))
     app.run(host=HOST, port=PORT, threaded=True)
-
