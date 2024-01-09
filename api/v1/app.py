812cb1e8a1b3483788013c72d3871a26eea75612
SALMAN BEN OMAR <salmanbenomar250@gmail.com>
2024-01-09 01:10:23 +0100

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -31,7 +31,7 @@ def not_found(error):
 if __name__ == "__main__":
     """ Main Function """
     host = environ.get('HBNB_API_HOST')
-    port = environ.get('HBNB_API_PORT')
+    port = int(environ.get('HBNB_API_PORT', 5000))
     if not host:
         host = '0.0.0.0'
     if not port:
