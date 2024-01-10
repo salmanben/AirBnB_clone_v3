f6789d2f8bf1c4d6ec395fa6161cc85261e07128
SALMAN BEN OMAR <salmanbenomar250@gmail.com>
2024-01-10 03:42:46 +0100

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-""" Index """
+""" Defienes status route """
 from models.amenity import Amenity
+from api.v1.views import app_views
+from flask import jsonify
+from models import storage
 from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
-from models import storage
-from api.v1.views import app_views
-from flask import jsonify
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
