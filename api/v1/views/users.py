dbe6bc298fbc267f25c8f51aa3c2ea49e8457d0d
MustaSteve <125991414+MustaSteve@users.noreply.github.com>
2024-01-10 03:53:01 +0100

    Update users.py

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-""" objects that handle all default RestFul API actions for Users """
-from models.user import User
+""" objects that handle all default RestFul API actions """
+
 from models import storage
+from models.user import User
 from api.v1.views import app_views
 from flask import abort, jsonify, make_response, request
 from flasgger.utils import swag_from
