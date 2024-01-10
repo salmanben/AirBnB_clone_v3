1428d963a81b1b428d40a4bb43ff464aa147ea3a
MustaSteve <125991414+MustaSteve@users.noreply.github.com>
2024-01-10 03:53:44 +0100

    Update states.py

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-""" objects that handle all default RestFul API actions for States """
+""" objects that handle all default RestFul """
+from api.v1.views import app_views
 from models.state import State
 from models import storage
-from api.v1.views import app_views
 from flask import abort, jsonify, make_response, request
 from flasgger.utils import swag_from
 
